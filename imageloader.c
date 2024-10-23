65fba839fea8673ecb1d4e76edbe1c4d903a0d2b
Chris <2293611667@qq.com>
2024-10-24 01:18:55 +0800

    Finish the pro1-partA

--- imageloader.c
+++ imageloader.c
@@ -25,17 +25,72 @@
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
 {
-	//YOUR CODE HERE
+	FILE * fp ;
+	if((fp = fopen(filename,"r") ) == NULL)
+	{
+		perror("fopen\n");
+		exit(-1);
+	}
+	char op[2];int rows,cols,Maxp;
+	fscanf(fp,"%s%d%d%d", op,&rows,&cols,&Maxp);
+	if(op[0] != 'P' || op[1] != '3' )
+	{
+		perror("ppm error\n");
+		exit(-1);
+	}
+	Image* ret = (Image* ) malloc(sizeof (struct Image));
+	Color** images = (Color**)malloc(sizeof(void*)* rows * cols);
+	for(int i = 0 ;i < rows  ; i++)
+		for(int j = 0; j<cols ;	j++)
+		{
+			int r,g,b;
+			fscanf(fp,"%d%d%d",&r,&g,&b);
+			if(0<=r && r <= 255 && 0<=g && g<=255 && 0<= b && b<=255)
+			{
+				Color* color = (Color *)malloc(sizeof(struct Color)); 
+				color->R = (uint8_t)r;
+				color->G = (uint8_t)g;
+				color->B = (uint8_t)b; 
+			 	images[j + i * cols ] = color;	
+			}
+			else perror("cur range out\n");
+		}
+	ret->image = images;
+	ret->rows = rows;
+	ret->cols = cols;
+	return ret;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	uint8_t rows = image->rows;
+	uint8_t cols = image->cols;
+	Color** colors = image-> image;
+	printf("%s\n%d %d\n%d\n","P3",rows,cols,255);
+	for(int i = 0 ; i<rows; i++)
+	{
+		for(int j = 0 ; j<cols ;j++)
+		{
+			struct Color * color = colors[j + i * cols];
+			printf("%3u %3u %3u ",color->R,color->G,color->B);
+		} 
+		putc('\n',stdout);
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+	Color** colors = image->image;
+	int rows = image -> rows;
+	int cols = image -> cols;
+	for(int i = 0 ;i < rows; i++)
+		for(int j = 0; j< cols ;j++)
+		{
+			struct Color * color = colors[j+ i*cols];
+			free(color);
+		}
+	free(image);
 }
+	
--- steganography.c
+++ steganography.c
@@ -21,14 +21,25 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+	Color** colors = image->image;
+	Color*  color = colors[col+ row * (image->rows)];
+	Color* ret = (Color* )malloc(sizeof(Color));
+	ret->R = ret->G = ret->B = (uint8_t) ((color->B & 1)  == 0 ? 0 : 255); 
+	return ret;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
-}
+	int rows = image->rows , cols= image-> cols;
+	Image* ret = (Image* )malloc(sizeof (struct Image));
+	ret->image = (Color**) malloc(sizeof(void*) * rows * cols );
+	ret->rows = rows; ret->cols =cols;
+	for(int i = 0 ;i < rows ;i++)
+		for(int j = 0 ; j< cols ; j++){
+			ret->image[j+ i * rows] = evaluateOnePixel(image,i,j);}
+	return ret;
+}	
 
 /*
 Loads a file of ppm P3 format from a file, and prints to stdout (e.g. with printf) a new image, 
@@ -45,5 +56,28 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	if(argc < 2){  perror("argc < 2\n"); exit(-1) ; }
+	char* filename = argv[1];
+	Image* oriImage  = readData(filename);
+	Image* transImage = steganography(oriImage);
+	writeData(transImage);
+	Color** colors = transImage->image;
+	for(int i = 0 ;i < transImage->rows; i++)
+		for(int j = 0; j< transImage->cols ;j++)
+		{
+			struct Color * color = colors[j + i * transImage->cols];
+			free(color);
+		}
+	colors = oriImage->image;
+	for(int i = 0 ;i < oriImage->rows; i++)
+		for(int j = 0; j< oriImage->cols ;j++)
+		{
+			struct Color * color = colors[j + i * oriImage->cols];
+			free(color);
+		}
+	free(transImage->image);
+	free(oriImage ->image);
+	free(transImage);
+	free(oriImage);
+	return 0;
 }
